d8a4188609ea7f08a2640ab0622f8f6f77a1664a
washingtonmoreiraevangelista <washingtonmoreiraevangelista@gmail.com>
2025-05-07 19:17:03 -0300

    feate:o mano o

--- Projeto node puro/src/middlewares/json.js
+++ Projeto node puro/src/middlewares/json.js
@@ -6,7 +6,7 @@ export async function json(req, res) {
   }
 
   try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
+    req.body = JSON.parse(buffers.concat(buffers).toString())
   } catch {
     req.body = null
   }
